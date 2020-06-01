b12ee6ba28182619587ff74303295e5ad30232ca
Teddy Bradsher <teddybradsher@gmail.com>
2020-06-01 05:53:27 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,17 @@
 def square_array(array)
+<<<<<<< HEAD
  number = []
  array.each do |squared|
   number << squared ** 2
  end
  return number
+=======
+
+ new_array = []
+
+ array.each do |integer|
+  new_array << integer ** 2
+ end
+ return new_array
+>>>>>>> 68be00a8d32fce532898e420b04935b16b0011af
 end
